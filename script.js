3dec42821d631bd66d3068768e69d88b3dbdbb9b
OyinOlad <131097554+OyinOlad@users.noreply.github.com>
2023-06-05 04:41:21 -0230

    yes

---
+++ script.js
@@ -0,0 +1,31 @@
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+let header = document.querySelector('header');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+        }
+    });
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .portfolio-box', { origin: 'bottom' });
